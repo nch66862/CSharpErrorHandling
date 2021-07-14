7b12c43783d28983c5c0414628df1e1950fc746f
Nick Carver <nicholascarver2@outlook.com>
2021-07-13 21:42:41 -0500

    added a gitignore and an exceptions library

--- ErrorHandlingProgram/Program.cs
+++ ErrorHandlingProgram/Program.cs
@@ -7,6 +7,8 @@ namespace CSharpErrorHandling
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
